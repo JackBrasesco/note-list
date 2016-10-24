d978a09318a8cd6d440067f6a071e8bab0ef7ba3
JackBrasesco <jacbras@nuevaschool.org>
2016-10-24 09:05:39 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ noteInput.keydown(function(e) {
 function getNotesForName(name) {  
   onNewListItem(name + "/notes", function(note) {
     console.log(note)
+    noteList.html(note)
   })
 }
+
+var notes = []
 
